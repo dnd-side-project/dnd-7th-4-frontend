71845e2ba670ba6ac8181a9a256487a324040932
L2HYUNN <dhl9810@naver.com>
2022-08-07 04:59:35 +0900

    feat(moreweatherinfo): add more weather info
    더보기 날씨 정보 추가 ( mid - fi )

---
+++ src/components/MoreWeatherInfo/MoreWeatherInfo.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: grid;
+  width: 31.2rem;
+  height: 60rem;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  gap: 1.1rem;
+`;
+
+export { Wrapper };
