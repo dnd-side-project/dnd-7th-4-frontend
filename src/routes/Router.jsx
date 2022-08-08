3a1b54cdc5d591ba31ed530b844a2951b6310329
Haena <kimhn200605@gmail.com>
2022-08-08 19:41:45 +0900

    feat(routes): add location page route

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Routers, Routes, Route } from 'react-router-dom';
 import ExamplePage from '@Pages/ExamplePage';
+import LocationPage from '@Pages/LocationPage';
 
 function Router() {
   return (
     <Routers>
       <Routes>
         <Route path="/" element={<ExamplePage />} />
+        <Route path="/location" element={<LocationPage />} />
       </Routes>
     </Routers>
   );
