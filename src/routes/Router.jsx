8719a9ff3331cf3cd9f43ac3ba0c4c1234b2359b
zeroto99 <myv_01@naver.com>
2022-08-08 18:56:36 +0900

    feat(routes): add home page route

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Routers, Routes, Route } from 'react-router-dom';
 import ExamplePage from '@Pages/ExamplePage';
+import HomePage from '@Pages/HomePage';
 
 function Router() {
   return (
     <Routers>
       <Routes>
-        <Route path="/" element={<ExamplePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/example" element={<ExamplePage />} />
       </Routes>
     </Routers>
   );
