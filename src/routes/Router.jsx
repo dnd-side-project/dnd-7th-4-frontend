fefcc7881c37e138c89b36d13dbcb3559094445d
Haena <kimhn200605@gmail.com>
2022-08-14 18:17:41 +0900

    feat(routes): add login routes

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Routers, Routes, Route } from 'react-router-dom';
 import HomePage from '@Pages/HomePage';
 import ExamplePage from '@Pages/ExamplePage';
 import LocationPage from '@Pages/LocationPage';
-
+import LoginPage from '@Pages/LoginPage';
+import Auth from '../Auth';
 
 function Router() {
   return (
@@ -11,6 +12,8 @@ function Router() {
         <Route path="/" element={<HomePage />} />
         <Route path="/location" element={<LocationPage />} />
         <Route path="/example" element={<ExamplePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/oauth/kakao/callback" element={<Auth />} />
       </Routes>
     </Routers>
   );
