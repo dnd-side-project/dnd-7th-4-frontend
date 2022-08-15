41e83a90c061bfc5a71db863514ba0b00d2e0468
L2HYUNN <dhl9810@naver.com>
2022-08-16 07:30:30 +0900

    feat(api): add api example

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,26 @@
-const BASE_URL = '#';
+/* eslint-disable consistent-return */
+import axios from 'axios';
 
-export default BASE_URL;
+// const BASE_URL = 'https://weathertogo.shop';
+const TEST_URL = 'https://reqres.in';
+
+const fetchTestInfo = async () => {
+  try {
+    const response = await axios.get(`${TEST_URL}/api/users/2`);
+    return response.data;
+  } catch (error) {
+    console.log('Error: ', error);
+  }
+};
+
+// axios.get();
+
+// axios.post();
+
+// axios.put();
+
+// axios.patch();
+
+// axios.delete();
+
+export { fetchTestInfo };
--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,13 +1,29 @@
 // import * as S from './WeatherForecast.style';
-import { useRecoilValue } from 'recoil';
+import { fetchTestInfo } from '@Apis/api';
 import tabMenuAtom from '@Recoil/tabMenu';
+import { useQuery } from '@tanstack/react-query';
+import { useRecoilValue } from 'recoil';
+
+import ForecastTab from './ForecastTab';
+import ThisWeek from './ThisWeek';
 import Today from './Today';
 import Tomorrow from './Tomorrow';
-import ThisWeek from './ThisWeek';
-import ForecastTab from './ForecastTab';
+
+// CHECK:: 다른 폴더나 파일로 빼는 게 나을지 고민
+const obj = {
+  0: <Today />,
+  1: <Tomorrow />,
+  2: <ThisWeek />,
+};
 
 const WeatherForecast = () => {
   const active = useRecoilValue(tabMenuAtom);
+  // console.log(fetchTestInfo());
+  const { isLoading, data } = useQuery(['test'], fetchTestInfo);
+
+  if (!isLoading) {
+    console.log(data);
+  }
 
   return (
     <main>
@@ -15,13 +31,6 @@ const WeatherForecast = () => {
       <section>{obj[active]}</section>
     </main>
   );
-};
-
-// CHECK:: 다른 폴더나 파일로 빼는 게 나을지 고민
-const obj = {
-  0: <Today />,
-  1: <Tomorrow />,
-  2: <ThisWeek />,
 };
 
 export default WeatherForecast;
