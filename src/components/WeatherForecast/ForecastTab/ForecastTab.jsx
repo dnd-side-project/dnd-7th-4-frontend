46ba672a222e0cb09c644b812871c80f7a21c138
zeroto99 <myv_01@naver.com>
2022-08-16 01:44:14 +0900

    feat(components): add main function

--- src/components/WeatherForecast/ForecastTab/ForecastTab.jsx
+++ src/components/WeatherForecast/ForecastTab/ForecastTab.jsx
@@ -1,23 +1,23 @@
 import * as S from './ForecastTab.style';
 import { memo } from 'react';
 import isEqual from 'react-fast-compare';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 import tabMenuAtom from '@Recoil/tabMenu';
 
 const ForecastTab = () => {
-  const setActive = useSetRecoilState(tabMenuAtom);
+  const [active, setActive] = useRecoilState(tabMenuAtom);
+  const content = ['오늘', '내일', '이번주'];
 
   return (
     <S.ForecastTabList>
-      <li>
-        <button onClick={() => setActive(0)}>오늘</button>
-      </li>
-      <li>
-        <button onClick={() => setActive(1)}>내일</button>
-      </li>
-      <li>
-        <button onClick={() => setActive(2)}>이번주</button>
-      </li>
+      {content.map((item, idx) => (
+        // CHECK:: content 배열의 요소들이 변경되거나 재배치될 일이 없기 때문에 index를 key값으로 사용했다. 옳은 선택인지?
+        <li key={idx}>
+          <button className={active === idx ? 'on' : null} onClick={() => setActive(idx)}>
+            {item}
+          </button>
+        </li>
+      ))}
     </S.ForecastTabList>
   );
 };
