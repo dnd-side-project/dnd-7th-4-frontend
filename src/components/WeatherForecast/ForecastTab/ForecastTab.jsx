2533eb78c2584d8afe317e9067504b1c9fe89b91
JiAh Lee <myv_01@naver.com>
2022-08-18 02:06:05 +0900

    feat(components): apply hi-fi design

--- src/components/WeatherForecast/ForecastTab/ForecastTab.jsx
+++ src/components/WeatherForecast/ForecastTab/ForecastTab.jsx
@@ -10,18 +10,18 @@ const ForecastTab = () => {
   const content = ['오늘', '내일', '이번주'];
 
   return (
-    <S.ForecastTabList>
+    <S.TabList>
       {content.map((item, idx) => (
         // CHECK:: content 배열의 요소들이 변경되거나 재배치될 일이 없기 때문에 index를 key값으로 사용했다. 옳은 선택인지?
         // eslint-disable-next-line react/no-array-index-key
         <li key={idx}>
           {/* eslint-disable-next-line react/button-has-type */}
-          <button className={active === idx ? 'on' : null} onClick={() => setActive(idx)}>
+          <S.Tab className={active === idx ? 'on' : null} onClick={() => setActive(idx)}>
             {item}
-          </button>
+          </S.Tab>
         </li>
       ))}
-    </S.ForecastTabList>
+    </S.TabList>
   );
 };
 
