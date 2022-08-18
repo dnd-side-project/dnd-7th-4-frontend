76ed6dc3d8054dc70fd9ecaf5a567b5f53b672f7
L2HYUNN <dhl9810@naver.com>
2022-08-18 09:41:48 +0900

    refactor: add error hanlder base using react-query
    react-query를 이용한 error handler를 추가합니다.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
@@ -7,7 +7,14 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      console.log('Error: ', error);
+      console.log('Query: ', query);
+    },
+  }),
+});
 
 root.render(
   <React.StrictMode>
