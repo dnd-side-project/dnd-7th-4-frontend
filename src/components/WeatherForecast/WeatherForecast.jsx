f1da9bb2523c43ef1d22f0bc2a2ffc40d1fe065e
JiAh Lee <myv_01@naver.com>
2022-08-18 20:53:47 +0900

    refactor(components): remove useless code

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,43 +1,17 @@
-// import * as S from './WeatherForecast.style';
-import { fetchTestInfo } from '@Apis/api';
-import tabMenuAtom from '@Recoil/tabMenu';
-import { useQuery } from '@tanstack/react-query';
-import { memo, useMemo } from 'react';
+// import { fetchTestInfo } from '@Apis/api';
+// import tabMenuAtom from '@Recoil/tabMenu';
+// import { useQuery } from '@tanstack/react-query';
+import { memo } from 'react';
 import isEqual from 'react-fast-compare';
-import { useRecoilValue } from 'recoil';
 
+import Days from './Days';
 import ForecastTab from './ForecastTab';
-import ThisWeek from './ThisWeek';
-import Today from './Today';
-import Tomorrow from './Tomorrow';
+// import * as S from './WeatherForecast.style';
 
-const content = {
-  0: <Today />,
-  1: <Tomorrow />,
-  2: <ThisWeek />,
-};
-
-const WeatherForecast = () => {
-  const active = useRecoilValue(tabMenuAtom);
-  // console.log(fetchTestInfo());
-  const { isLoading, data } = useQuery(['test'], fetchTestInfo);
-
-  if (!isLoading) {
-    // eslint-disable-next-line no-console
-    console.log(data);
-  }
-
-  // eslint-disable-next-line arrow-body-style
-  const getContent = useMemo(() => {
-    return content[active];
-  }, [active]);
-
-  return (
-    <main>
-      <ForecastTab />
-      <section>{getContent}</section>
-    </main>
-  );
-};
-
+const WeatherForecast = () => (
+  <main>
+    <ForecastTab />
+    <Days />
+  </main>
+);
 export default memo(WeatherForecast, isEqual);
