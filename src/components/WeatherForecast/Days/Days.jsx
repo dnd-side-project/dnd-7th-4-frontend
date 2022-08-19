8dcb803e9d45f5bba45d13b2989d1880d8a701e7
JiAh Lee <myv_01@naver.com>
2022-08-20 03:13:19 +0900

    feat(components): apply api data

--- src/components/WeatherForecast/Days/Days.jsx
+++ src/components/WeatherForecast/Days/Days.jsx
@@ -1,147 +1,62 @@
-/* eslint-disable */
 import testImg from '@Assets/icon/logo-symbol.svg';
+import tabMenuAtom from '@Recoil/tabMenu';
+import { weatherWithSelect } from '@Recoil/weather';
 import { memo } from 'react';
 import isEqual from 'react-fast-compare';
-import tabMenuAtom from '@Recoil/tabMenu';
 import { useRecoilValue } from 'recoil';
 
-import * as S from './Days.style';
 import ThisWeek from '../ThisWeek';
-
-const today = [
-  {
-    최저기온: '22.0',
-    최고기온: '29.0',
-    기온: '22',
-    차이: '1',
-    멘트: '아 맞다! 우산!/잊지말고 챙기세요!',
-    캡션: '자외선 지수 높음',
-  },
-];
-
-const tomorrow = [
-  {
-    최저기온: '24.0',
-    최고기온: '30.0',
-    기온: '27',
-    차이: '1',
-    멘트: '오후 외출시/우산을 챙기세요!',
-    캡션: '자외선 지수 매우 높음',
-  },
-];
-
-const arr2 = [
-  { 시간: '지금', 기온: '28' },
-  { 시간: '오후 3시', 기온: '28' },
-  { 시간: '오후 4시', 기온: '28' },
-  { 시간: '오후 5시', 기온: '28' },
-  { 시간: '오후 6시', 기온: '28' },
-  { 시간: '오후 7시', 기온: '28' },
-  { 시간: '오후 8시', 기온: '28' },
-  { 시간: '오후 9시', 기온: '28' },
-  { 시간: '오후 10시', 기온: '28' },
-  { 시간: '오후 11시', 기온: '28' },
-  { 시간: '오후 12시', 기온: '28' },
-];
+import * as S from './Days.style';
 
 const Days = () => {
-  const active = useRecoilValue(tabMenuAtom);
+  const tabName = useRecoilValue(tabMenuAtom);
+  const content = useRecoilValue(weatherWithSelect);
 
-  {
-    if (active === 0) {
-      return (
+  if (tabName !== '이번주') {
+    return (
+      content && (
         <>
-          {today.map((item, idx) => (
-            <div key={idx}>
-              <S.InfList>
-                <S.InfListItem minmax>
-                  <S.Wrapper>
-                    <S.Item>{item.최저기온.split('.')[0]}º</S.Item>
-                    <S.Item divider>/</S.Item>
-                    <S.Item>{item.최고기온.split('.')[0]}º</S.Item>
-                  </S.Wrapper>
-                </S.InfListItem>
-                <S.InfListItem temp>
-                  <S.Wrapper>
-                    <S.Item>{item.기온}</S.Item>
-                    <S.Item sign>º</S.Item>
-                  </S.Wrapper>
-                </S.InfListItem>
-                <S.InfListItem diff>어제 +{item.차이}º</S.InfListItem>
-              </S.InfList>
-              <S.CommentWrapper>
-                <S.Comment>
-                  <li>{item.멘트.split('/')[0]}</li>
-                  <li>{item.멘트.split('/')[1]}</li>
-                </S.Comment>
-                <S.Caption>{item.캡션}</S.Caption>
-              </S.CommentWrapper>
-            </div>
-          ))}
+          <S.InfList>
+            <S.InfListItem minmax>
+              <S.Wrapper>
+                <S.Item>{content['현재']['최저기온'].split('.')[0]}º</S.Item>
+                <S.Item divider>/</S.Item>
+                <S.Item>{content['현재']['최고기온'].split('.')[0]}º</S.Item>
+              </S.Wrapper>
+            </S.InfListItem>
+            <S.InfListItem temp>
+              <S.Wrapper>
+                <S.Item>{content['현재']['기온']}</S.Item>
+                <S.Item sign>º</S.Item>
+              </S.Wrapper>
+            </S.InfListItem>
+            <S.InfListItem diff>어제 {content['전날기온차이']}º</S.InfListItem>
+          </S.InfList>
+          <S.CommentWrapper>
+            <S.Comment>
+              <li>{content['세부코멘트']['메인']['코멘트'].split('/')[0]}</li>
+              <li>{content['세부코멘트']['메인']['코멘트'].split('/')[1]}</li>
+            </S.Comment>
+            <S.Caption>{content['세부코멘트']['메인']['캡션']}</S.Caption>
+          </S.CommentWrapper>
           <S.Timeline>
-            {arr2.map((it, idx) => (
-              <li key={idx}>
+            {Object.keys(content['시간별정보']).map((item) => (
+              <li key={item}>
                 <S.ItemList>
-                  <li>{it.시간}</li>
+                  <li>{item}시</li>
                   <li>
                     <img src={testImg} alt="구름 조금 아이콘" />
                   </li>
-                  <li>{it.기온}º</li>
+                  <li>{content['시간별정보'][item][0]}º</li>
                 </S.ItemList>
               </li>
             ))}
           </S.Timeline>
         </>
-      );
-    } else if (active === 1) {
-      return (
-        <>
-          {tomorrow.map((item, idx) => (
-            <div key={idx}>
-              <S.InfList>
-                <S.InfListItem minmax>
-                  <S.Wrapper>
-                    <S.Item>{item.최저기온.split('.')[0]}º</S.Item>
-                    <S.Item divider>/</S.Item>
-                    <S.Item>{item.최고기온.split('.')[0]}º</S.Item>
-                  </S.Wrapper>
-                </S.InfListItem>
-                <S.InfListItem temp>
-                  <S.Wrapper>
-                    <S.Item>{item.기온}</S.Item>
-                    <S.Item sign>º</S.Item>
-                  </S.Wrapper>
-                </S.InfListItem>
-                <S.InfListItem diff>어제 +{item.차이}º</S.InfListItem>
-              </S.InfList>
-              <S.CommentWrapper>
-                <S.Comment>
-                  <li>{item.멘트.split('/')[0]}</li>
-                  <li>{item.멘트.split('/')[1]}</li>
-                </S.Comment>
-                <S.Caption>{item.캡션}</S.Caption>
-              </S.CommentWrapper>
-            </div>
-          ))}
-          <S.Timeline>
-            {arr2.map((it, idx) => (
-              <li key={idx}>
-                <S.ItemList>
-                  <li>{it.시간}</li>
-                  <li>
-                    <img src={testImg} alt="구름 조금 아이콘" />
-                  </li>
-                  <li>{it.기온}º</li>
-                </S.ItemList>
-              </li>
-            ))}
-          </S.Timeline>
-        </>
-      );
-    } else {
-      return <ThisWeek />;
-    }
+      )
+    );
   }
+  return <ThisWeek />;
 };
 
 export default memo(Days, isEqual);
--- src/components/WeatherForecast/ThisWeek/ThisWeek.jsx
+++ src/components/WeatherForecast/ThisWeek/ThisWeek.jsx
@@ -1,49 +1,48 @@
 import testImg from '@Assets/icon/logo-symbol.svg';
+import { weatherWithSelect } from '@Recoil/weather';
 import { memo } from 'react';
 import isEqual from 'react-fast-compare';
+import { useRecoilValue } from 'recoil';
 
 import * as S from './ThisWeek.style';
 
-const arr = [
-  { 요일: '오늘', 날짜: '7/38', 최저온도: '22', 최고온도: '28' },
-  { 요일: '화', 날짜: '7/38', 최저온도: '22', 최고온도: '28' },
-  { 요일: '수', 날짜: '7/38', 최저온도: '22', 최고온도: '28' },
-  { 요일: '목', 날짜: '7/38', 최저온도: '22', 최고온도: '28' },
-  { 요일: '금', 날짜: '7/38', 최저온도: '22', 최고온도: '28' },
-  { 요일: '토', 날짜: '7/38', 최저온도: '22', 최고온도: '28' },
-  { 요일: '일', 날짜: '7/38', 최저온도: '22', 최고온도: '28' },
-];
+const ThisWeek = () => {
+  const content = useRecoilValue(weatherWithSelect);
+  // eslint-disable-next-line no-console
+  console.log('content', content);
 
-const ThisWeek = () => (
-  <>
-    <S.CommentDiv>
-      <S.CommentTitle>화요일, 목요일에는 우산 챙기세요.</S.CommentTitle>
-    </S.CommentDiv>
-    <S.WeekList>
-      {arr.map((item, idx) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <li key={idx}>
-          <S.DayList>
-            <li>
-              <S.DateList>
-                <S.DateItem>{item.요일}</S.DateItem>
-                <S.DateItem size="true">{item.날짜}</S.DateItem>
-              </S.DateList>
-            </li>
-            <li>
-              <img src={testImg} alt="날씨 이모티콘" />
-            </li>
-            <li>
-              <img src={testImg} alt="날씨 이모티콘" />
-            </li>
-            <li>
-              {item.최저온도}º / {item.최고온도}º
-            </li>
-          </S.DayList>
-        </li>
-      ))}
-    </S.WeekList>
-  </>
-);
+  return (
+    <>
+      <S.CommentDiv>
+        <S.CommentTitle>화요일, 목요일에는 우산 챙기세요.</S.CommentTitle>
+      </S.CommentDiv>
+      <S.WeekList>
+        {Object.keys(content).map((item) => (
+          <li key={item}>
+            <S.DayList>
+              <li>
+                <S.DateList>
+                  <S.DateItem>오늘</S.DateItem>
+                  <S.DateItem size="true">일</S.DateItem>
+                </S.DateList>
+              </li>
+              <li>
+                <img src={testImg} alt="날씨 이모티콘" />
+                <S.Percent>{content[item]['오전강수확률']}%</S.Percent>
+              </li>
+              <li>
+                <img src={testImg} alt="날씨 이모티콘" />
+                <S.Percent>{content[item]['오후강수확률']}%</S.Percent>
+              </li>
+              <li>
+                {content[item]['최저기온'].split('.')[0]}º / {content[item]['최고기온'].split('.')[0]}º
+              </li>
+            </S.DayList>
+          </li>
+        ))}
+      </S.WeekList>
+    </>
+  );
+};
 
 export default memo(ThisWeek, isEqual);
