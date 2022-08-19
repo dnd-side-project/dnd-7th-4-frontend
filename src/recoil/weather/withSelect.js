15fedc9afd5ce003e180c4626cf724cc86b6685d
JiAh Lee <myv_01@naver.com>
2022-08-20 03:14:17 +0900

    refactor(recoil): add optional chaining

--- src/recoil/weather/withSelect.js
+++ src/recoil/weather/withSelect.js
@@ -7,16 +7,16 @@ const weatherWithSelect = selector({
   key: 'weatherWithSelect',
   get: ({ get }) => {
     const tabMenuName = get(tabMenuAtom);
-    let weather = get(weatherAtom).data?.오늘;
+    let weather = get(weatherAtom)?.data?.오늘;
     switch (tabMenuName) {
       case '오늘':
-        weather = get(weatherAtom).data?.오늘;
+        weather = get(weatherAtom)?.data?.오늘;
         break;
       case '내일':
-        weather = get(weatherAtom).data?.내일;
+        weather = get(weatherAtom)?.data?.내일;
         break;
       case '이번주':
-        weather = get(weatherAtom).data?.이번주;
+        weather = get(weatherAtom)?.data?.이번주;
         break;
       default:
     }
