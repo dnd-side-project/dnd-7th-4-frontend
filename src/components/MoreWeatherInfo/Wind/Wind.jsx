d8400aa02aca6fd955e41ea1ec8c827290d18b28
L2HYUNN <dhl9810@naver.com>
2022-08-20 19:26:16 +0900

    refactor(components): apply new design Wind
    API 연결 및 새로운 디자인 적용

--- src/components/MoreWeatherInfo/Wind/Wind.jsx
+++ src/components/MoreWeatherInfo/Wind/Wind.jsx
@@ -1,16 +1,26 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+/* eslint-disable unused-imports/no-unused-vars */
 import { memo } from 'react';
 import { isEqual } from 'react-fast-compare';
 
 import * as S from './Wind.style';
 
-const Wind = () => (
+const Wind = ({ data, status }) => (
   <S.Wrapper>
-    <S.Title>바람 2m/s</S.Title>
-    <S.Texts>
-      <S.Text>기분좋은</S.Text>
-      <S.Text>남실바람</S.Text>
-    </S.Texts>
-    {/* <S.Img></S.Img> */}
+    <S.Comments>
+      <S.Comment>{data?.코멘트}</S.Comment>
+    </S.Comments>
+    <S.Infos>
+      <S.Info>
+        <S.InfoTitle>바람</S.InfoTitle>
+        <S.InfoTexts>
+          <S.InfoText>{status}</S.InfoText>
+          <S.InfoUnit>m/s</S.InfoUnit>
+        </S.InfoTexts>
+      </S.Info>
+      <S.Img src={data?.이미지url} alt="바람 아이콘" />
+    </S.Infos>
   </S.Wrapper>
 );
 
