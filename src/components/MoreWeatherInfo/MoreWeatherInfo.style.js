3f668c6b2a47e9f153d8a97d58b6b77ccfdae528
L2HYUNN <dhl9810@naver.com>
2022-08-20 19:15:40 +0900

    refactor(components): apply new design
    API 연결 및 새로운 디자인 적용

--- src/components/MoreWeatherInfo/MoreWeatherInfo.style.js
+++ src/components/MoreWeatherInfo/MoreWeatherInfo.style.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
+const Wrapper = styled.section`
   display: grid;
-  width: 31.2rem;
-  height: 60rem;
+  width: 31.1rem;
+  height: 57.6rem;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: repeat(3, 1fr);
   gap: 1.1rem;
+  margin: 0 auto;
 `;
 
 export { Wrapper };
