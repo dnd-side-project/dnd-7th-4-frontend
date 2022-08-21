ed5c489e475523c67f84435bd4d8aded199d0282
Haena <kimhn200605@gmail.com>
2022-08-21 10:01:14 +0900

    refactor(components): apply new design to LocationResult

--- src/components/Location/LocationResult/LocationResult.jsx
+++ src/components/Location/LocationResult/LocationResult.jsx
@@ -1,108 +1,87 @@
+import { getLocationData } from '@Apis/api';
 import locationEditAtom from '@Recoil/location/locationEdit';
 import locationValueAtom from '@Recoil/location/locationValue';
+import { useQuery } from '@tanstack/react-query';
 import { memo, useEffect, useState } from 'react';
 import isEqual from 'react-fast-compare';
 import { useRecoilValue } from 'recoil';
 
+// import { ReactComponent as deleteIcon } from '../../../assets/icon/delete.svg';
 import * as S from './LocationResult.style';
 
 const LocationResult = () => {
   const [pinned, setPinned] = useState([]);
   const [places, setPlaces] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [region, setRegion] = useState('');
 
   const value = useRecoilValue(locationValueAtom);
   const edit = useRecoilValue(locationEditAtom);
+
+  const { isLoading, data } = useQuery(['weather'], () => getLocationData({ city: '서울특별시', district: '종로구' }));
+
+  const DeleteRegion = () => {
+    setModal(false);
+  };
+  useEffect(() => {
+    if (!isLoading) {
+      setPlaces(data);
+    }
+  }, [isLoading, data]);
 
   // PinnedPlaces, SearchPlace 임시 더미 데이터
   useEffect(() => {
     setPinned([
       {
         id: 1,
-        name: '서울 마포구',
+        name: '서울시 마포구',
         temp: 25,
         word: '맑음',
       },
       {
         id: 2,
-        name: '서울 강동구',
-        temp: 30,
-        word: '약한 비',
-      },
-      {
-        id: 3,
         name: '경기도 일산동구',
-        temp: 22,
-        word: '강한 비',
-      },
-      {
-        id: 4,
-        name: '서울시 동작구',
-        temp: 31,
+        temp: 25,
         word: '맑음',
-      },
-    ]);
-
-    setPlaces([
-      {
-        id: 1,
-        name: '서울',
-        icon: 'Icon',
-        temp: 29,
-      },
-      {
-        id: 2,
-        name: '서울, 도봉구',
-        icon: 'Icon',
-        temp: 31,
-      },
-      {
-        id: 3,
-        name: '서울, 동대문구',
-        icon: 'Icon',
-        temp: 29,
-      },
-      {
-        id: 4,
-        name: '서울, 동작구',
-        icon: 'Icon',
-        temp: 26,
-      },
-      {
-        id: 5,
-        name: '서울, 은평구',
-        icon: 'Icon',
-        temp: 28,
       },
     ]);
   }, []);
 
   return (
-    <S.LocationResult>
-      {!value
-        ? // 검색 이전 띄워주는 창 (저장된 장소 리스트)
-          pinned.map((place) => (
-            <S.LocationPinnedPlace
-              key={place.id}
-              name={place.name}
-              word={place.word}
-              temp={place.temp}
-              value={value}
-              edit={edit}
-            >
-              <S.LocationPinnedName>{place.name}</S.LocationPinnedName>
-              <S.LocationPinnedTemp>{place.temp}</S.LocationPinnedTemp>
-              <S.LocationPinnedWord>{place.word}</S.LocationPinnedWord>
-            </S.LocationPinnedPlace>
-          ))
-        : // 장소 검색 결과 띄워주는 창
-          places.map((result) => (
-            <S.LocationSearchPlace key={result.id} name={result.name} icon={result.icon} temp={result.temp}>
-              <S.LocationSearchPlaceName>{result.name}</S.LocationSearchPlaceName>
-              <S.LocationSearchPlaceIcon>{result.icon}</S.LocationSearchPlaceIcon>
-              <S.LocationSearchPlaceTemp>{result.temp}</S.LocationSearchPlaceTemp>
-            </S.LocationSearchPlace>
-          ))}
-    </S.LocationResult>
+    <>
+      <S.LocationModalWrapper modal={modal}>
+        {modal ? (
+          <S.LocationModal modal={modal}>
+            <S.LocationModalQuestion>`{region}`를 삭제하시겠습니까?</S.LocationModalQuestion>
+            <S.LocationModalCancel onClick={() => setModal(false)}>취소</S.LocationModalCancel>
+            <S.LocationModalDelete onClick={DeleteRegion}>삭제</S.LocationModalDelete>
+          </S.LocationModal>
+        ) : null}
+      </S.LocationModalWrapper>
+      <S.LocationResult modal={modal}>
+        {!value
+          ? // 검색 이전 띄워주는 창 (저장된 장소 리스트)
+            pinned.map((place) => (
+              <S.LocationPinnedPlace onClick={() => setRegion(place.name)} key={place.id} value={value}>
+                <S.LocationPinnedName showDeleteIcon={edit && region === place.name} edit={edit}>
+                  {place.name}
+                </S.LocationPinnedName>
+                <S.LocationPinnedTemp>{place.temp}</S.LocationPinnedTemp>
+                {edit && region === place.name ? (
+                  <S.LocationDeleteIcon modal={modal} onClick={() => setModal(true)} />
+                ) : null}
+              </S.LocationPinnedPlace>
+            ))
+          : // 장소 검색 결과 띄워주는 창
+            places.map((result) => (
+              <S.LocationSearchPlace key={result.id} name={result.name} icon={result.icon} temp={result.temp}>
+                <S.LocationSearchPlaceName>{result.name}</S.LocationSearchPlaceName>
+                <S.LocationSearchPlaceIcon>{result.icon}</S.LocationSearchPlaceIcon>
+                <S.LocationSearchPlaceTemp>{result.temp}</S.LocationSearchPlaceTemp>
+              </S.LocationSearchPlace>
+            ))}
+      </S.LocationResult>
+    </>
   );
 };
 
--- src/components/Location/LocationResult/LocationResult.style.js
+++ src/components/Location/LocationResult/LocationResult.style.js
@@ -1,11 +1,22 @@
+import DeleteIcon from '@Assets/icon/delete.svg';
 import styled from 'styled-components';
 
+const LocationModalWrapper = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 36rem;
+  height: 78rem;
+  display: ${({ modal }) => (modal ? `show` : `none`)};
+  background: ${({ modal }) => (modal ? `rgba(0, 0, 0, 0.25); z-index:1;` : null)};
+`;
 const LocationResult = styled.main`
   display: flex;
   flex-direction: column;
   position: absolute;
   top: 18.9rem;
   width: 36rem;
+  ${({ modal }) => (modal ? `opacity: 0.2` : null)};
 `;
 const LocationPinnedPlace = styled.div`
   position: relative;
@@ -14,56 +25,46 @@ const LocationPinnedPlace = styled.div`
   margin: 0 auto;
   margin-bottom: 1.8rem;
   border-radius: 2rem;
-  background: #ffffff;
+  background: linear-gradient(180deg, #f2bb4f 73.39%, #f7c440 83.01%, #edd087 93.58%);
   box-shadow: 0rem 0rem 0.7rem rgba(0, 0, 0, 0.2);
   ${({ value }) => (value ? `display: hidden` : null)};
 `;
 const LocationPinnedName = styled.h1`
   display: flex;
   position: relative;
-  top: 1.5rem;
-  left: 1.2rem;
-  width: 14rem;
-  height: 1.7rem;
-  color: #000000;
-  font-family: 'Abel';
+  text-align: center;
+  justify-content: end;
+  top: 5.8rem;
+  margin-right: 1.6rem;
+  width: 12rem;
+  height: 1.6rem;
+  color: #ffffff;
+  font-family: 'Pretendard';
   font-style: normal;
-  font-weight: 400;
-  font-size: 1.7rem;
-  line-height: 1.7rem;
+  font-weight: 600;
+  font-size: 1.6rem;
+  line-height: 100%;
+  text-align: right;
   letter-spacing: 0.015rem;
+  text-shadow: 0rem 0.2rem 0.3rem rgba(0, 0, 0, 0.2);
+  left: ${({ showDeleteIcon }) => (showDeleteIcon ? `9.2rem;` : `16.8rem`)};
 `;
 const LocationPinnedTemp = styled.h1`
   display: flex;
   position: relative;
-  top: 2rem;
   left: 1.2rem;
-  width: 4.8rem;
-  height: 4rem;
-  color: #000000;
-  font-family: 'Roboto';
+  width: 7.3rem;
+  height: 6.3rem;
+  color: #ffffff;
+  font-family: 'Pretendard';
   font-style: normal;
-  font-weight: 400;
-  font-size: 4rem;
-  line-height: 4rem;
-  letter-spacing: 0.015rem;
+  font-weight: 700;
+  font-size: 5.8rem;
+  line-height: 100%;
+  text-shadow: 0rem 0.2rem 0.3rem rgba(0, 0, 0, 0.2);
   ::after {
     content: '°';
   }
-`;
-const LocationPinnedWord = styled.h1`
-  position: relative;
-  top: 0.5rem;
-  width: 5rem;
-  height: 1.2rem;
-  left: 7.5rem;
-  color: #000000;
-  font-family: 'Noto Sans';
-  font-style: normal;
-  font-weight: 500;
-  font-size: 1.2rem;
-  line-height: 1.2rem;
-  letter-spacing: 0.015rem;
 `;
 const LocationSearchPlace = styled.div`
   top: 1.4rem;
@@ -111,12 +112,90 @@ const LocationSearchPlaceTemp = styled.h1`
     content: '°';
   }
 `;
-
+const LocationDeleteIcon = styled.button`
+  display: flex;
+  position: absolute;
+  width: 8.6rem;
+  height: 9rem;
+  left: 22.6rem;
+  top: 0rem;
+  border: none;
+  border-radius: 0 2rem 2rem 0;
+  background: #eb4c4c;
+  background-image: url(${DeleteIcon});
+  background-repeat: no-repeat;
+  background-position: center;
+  display: ${({ modal }) => (modal ? `none` : `show`)};
+`;
+const LocationModal = styled.div`
+  position: absolute;
+  top: 26rem;
+  left: 6.6rem;
+  width: 22.9rem;
+  height: 13.7rem;
+  border-radius: 1.5rem;
+  background: #ffffff;
+`;
+const LocationModalQuestion = styled.h1`
+  position: absolute;
+  top: 2.5rem;
+  left: 5rem;
+  width: 13.5rem;
+  height: 4.8rem;
+  color: #000000;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.6rem;
+  line-height: 150%;
+  text-align: center;
+  letter-spacing: 0.03rem;
+`;
+const LocationModalCancel = styled.button`
+  position: absolute;
+  text-align: center;
+  top: 9.3rem;
+  left: 3.5rem;
+  width: 5rem;
+  height: 2.1rem;
+  color: #000000;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.4rem;
+  line-height: 150%;
+  letter-spacing: 0.015rem;
+  border: none;
+  background: inherit;
+  opacity: 0.4;
+`;
+const LocationModalDelete = styled.button`
+  position: absolute;
+  top: 9.3rem;
+  left: 14.5rem;
+  width: 5rem;
+  height: 2.1rem;
+  color: #ef1d00;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.4rem;
+  line-height: 150%;
+  text-align: center;
+  letter-spacing: 0.015rem;
+  border: none;
+  background: inherit;
+`;
 export {
+  LocationDeleteIcon,
+  LocationModal,
+  LocationModalCancel,
+  LocationModalDelete,
+  LocationModalQuestion,
+  LocationModalWrapper,
   LocationPinnedName,
   LocationPinnedPlace,
   LocationPinnedTemp,
-  LocationPinnedWord,
   LocationResult,
   LocationSearchPlace,
   LocationSearchPlaceIcon,
