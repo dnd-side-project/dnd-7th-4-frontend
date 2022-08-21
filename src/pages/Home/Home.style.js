122e9c8d8d95344d158476350a870213aeff9819
JiAh Lee <myv_01@naver.com>
2022-08-21 19:50:33 +0900

    feat(pages): apply selector data

--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 36rem;
   color: #fff;
-  background: ${({ theme }) => theme.skyColors.sunny};
   position: relative;
+  background: ${({ skyState, theme }) =>
+      Object.keys(theme.weatherImg).includes(skyState) ? theme.weatherImg[skyState] : null},
+    ${({ skyState, theme }) => (Object.keys(theme.skyColors).includes(skyState) ? theme.skyColors[skyState] : null)};
 `;
