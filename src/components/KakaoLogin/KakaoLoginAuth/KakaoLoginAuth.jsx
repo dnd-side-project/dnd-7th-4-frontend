ec37010619c751bb998887a68a6b43c200c688ff
Haena <kimhn200605@gmail.com>
2022-08-21 10:09:04 +0900

    feat(components): add KakaoLogin auth

---
+++ src/components/KakaoLogin/KakaoLoginAuth/KakaoLoginAuth.jsx
@@ -0,0 +1,18 @@
+// import axios from 'axios';
+
+const KakaoLoginAuth = () => {
+  // const code = new URL(window.location.href).searchParams.get('code');
+  // async function SendCode() {
+  //   try {
+  //     const response = await axios.get('https://weathertogo.shop/account/kakao/', {
+  //       // nickname: 'nickname',
+  //       // token: 'kakao_access_token',
+  //     });
+  //     return response;
+  //   } catch (error) {
+  //     //
+  //   }
+  // }
+  // SendCode();
+};
+export default KakaoLoginAuth;
