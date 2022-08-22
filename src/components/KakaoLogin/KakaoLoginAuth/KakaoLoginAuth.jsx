775654d810babfbc23623a2701292a10d760ab1f
Haena <kimhn200605@gmail.com>
2022-08-23 00:49:06 +0900

    feat(components): receive token

--- src/components/KakaoLogin/KakaoLoginAuth/KakaoLoginAuth.jsx
+++ src/components/KakaoLogin/KakaoLoginAuth/KakaoLoginAuth.jsx
@@ -1,18 +1,28 @@
-// import axios from 'axios';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const KakaoLoginAuth = () => {
-  // const code = new URL(window.location.href).searchParams.get('code');
-  // async function SendCode() {
-  //   try {
-  //     const response = await axios.get('https://weathertogo.shop/account/kakao/', {
-  //       // nickname: 'nickname',
-  //       // token: 'kakao_access_token',
-  //     });
-  //     return response;
-  //   } catch (error) {
-  //     //
-  //   }
-  // }
-  // SendCode();
+  const code = new URL(window.location.href).searchParams.get('code');
+  const navigate = useNavigate();
+  useEffect(() => {
+    (async () => {
+      try {
+        await axios.get(`https://weathertogo.shop/account/kakao/oauth?code=${code}`).then((response) => {
+          const djangoToken = response.data.django_token.access;
+          const refreshToken = response.data.django_token.refresh;
+          const { nickname } = response.data;
+          window.localStorage.setItem('access', djangoToken);
+          window.localStorage.setItem('refresh', refreshToken);
+          window.localStorage.setItem('nickname', nickname);
+        });
+        navigate('/alram');
+      } catch (e) {
+        //
+      }
+    })();
+  }, []);
+  const nickname = window.localStorage.getItem('nickname');
+  return nickname;
 };
 export default KakaoLoginAuth;
