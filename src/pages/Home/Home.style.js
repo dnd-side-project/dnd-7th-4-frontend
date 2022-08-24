2f685e3cd0b6f6dd1dfad6f504e821803f70d080
JiAh Lee <myv_01@naver.com>
2022-08-24 19:41:30 +0900

    refactor(pages): apply changed theme name

--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -4,7 +4,6 @@ export const Container = styled.div`
   width: 36rem;
   color: #fff;
   position: relative;
-  background: ${({ skyState, theme }) =>
-      Object.keys(theme.weatherImg).includes(skyState) ? theme.weatherImg[skyState] : null},
-    ${({ skyState, theme }) => (Object.keys(theme.skyColors).includes(skyState) ? theme.skyColors[skyState] : null)};
+  background: ${({ skyState, theme }) => (theme.weatherImgs[skyState] ? theme.weatherImgs[skyState] : null)},
+    ${({ skyState, theme }) => (theme.skyColors[skyState] ? theme.skyColors[skyState] : null)};
 `;
