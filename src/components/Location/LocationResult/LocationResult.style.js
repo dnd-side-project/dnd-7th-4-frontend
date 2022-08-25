42d6260142be9bde69482223ba6134a37b8760b6
Haena <kimhn200605@gmail.com>
2022-08-26 07:08:42 +0900

    feat(components): add logic register & delete region

--- src/components/Location/LocationResult/LocationResult.style.js
+++ src/components/Location/LocationResult/LocationResult.style.js
@@ -2,12 +2,10 @@ import DeleteIcon from '@Assets/icon/delete.svg';
 import styled from 'styled-components';
 
 const LocationModalWrapper = styled.div`
+  display: ${({ modal }) => (modal ? `show` : `none`)};
   position: fixed;
-  left: 0;
-  top: 0;
   width: 36rem;
   height: 78rem;
-  display: ${({ modal }) => (modal ? `show` : `none`)};
   background: ${({ modal }) => (modal ? `rgba(0, 0, 0, 0.25); z-index:1;` : null)};
 `;
 const LocationResult = styled.main`
@@ -16,62 +14,89 @@ const LocationResult = styled.main`
   position: absolute;
   top: 18.9rem;
   width: 36rem;
-  ${({ modal }) => (modal ? `opacity: 0.2` : null)};
+  opacity: ${({ modal }) => (modal ? `opacity: 0.2` : null)};
 `;
 const LocationPinnedPlace = styled.div`
+  display: ${({ value }) => (value ? `hidden` : null)};
   position: relative;
   width: 31.2rem;
-  height: 9rem;
+  height: 9.3rem;
   margin: 0 auto;
   margin-bottom: 1.8rem;
   border-radius: 2rem;
-  box-shadow: 0rem 0rem 0.7rem rgba(0, 0, 0, 0.2);
-  ${({ value }) => (value ? `display: hidden` : null)};
+  box-shadow: 0.15rem 0.1rem 0rem rgba(0, 0, 0, 0.15);
+  background: url(${({ src }) => `${src}`});
+  ${({ edit }) => (edit ? `cursor: null; pointer-events: none;` : `cursor: pointer;`)}
+`;
+const LocationPinnedNowPlace = styled.div`
+  display: ${({ value }) => (value ? `hidden` : null)};
+  position: relative;
+  width: 31.2rem;
+  height: 9.3rem;
+  margin: 0 auto;
+  margin-bottom: 1.8rem;
+  border-radius: 2rem;
+  box-shadow: 0.15rem 0.1rem 0rem rgba(0, 0, 0, 0.15);
+  background: url(${({ src }) => `${src}`});
+  ${({ edit }) => (edit ? `cursor: null; pointer-events: none;` : `cursor: pointer;`)}// background: red
+`;
+const LocationNowRegion = styled.div`
+  position: absolute;
+  top: 1.8rem;
+  left: 19.9rem;
+  width: 10rem;
+  height: 2.6rem;
+  color: #ffffff;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 2.6rem;
+  line-height: 100%;
+  text-align: right;
+  letter-spacing: 0.0229412rem;
+  text-shadow: 0rem 0.2rem 0.3rem rgba(0, 0, 0, 0.2);
 `;
 const LocationPinnedName = styled.h1`
   display: flex;
-  position: relative;
   text-align: center;
   justify-content: end;
+  position: relative;
   top: 5.8rem;
-  margin-right: 1.6rem;
-  width: 12rem;
+  left: 15rem;
+  width: 15rem;
   height: 1.6rem;
+  margin-right: 1.6rem;
   color: #ffffff;
-  font-family: 'Pretendard';
   font-style: normal;
   font-weight: 600;
   font-size: 1.6rem;
-  line-height: 100%;
   text-align: right;
   letter-spacing: 0.015rem;
   text-shadow: 0rem 0.2rem 0.3rem rgba(0, 0, 0, 0.2);
-  left: ${({ showDeleteIcon }) => (showDeleteIcon ? `9.2rem;` : `16.8rem`)};
 `;
 const LocationPinnedTemp = styled.h1`
   display: flex;
   position: relative;
-  left: 1.2rem;
+  top: 0rem;
+  left: 1.7rem;
   width: 7.3rem;
   height: 6.3rem;
   color: #ffffff;
-  font-family: 'Pretendard';
   font-style: normal;
   font-weight: 700;
   font-size: 5.8rem;
-  line-height: 100%;
   text-shadow: 0rem 0.2rem 0.3rem rgba(0, 0, 0, 0.2);
   ::after {
     content: '°';
   }
 `;
 const LocationSearchPlace = styled.div`
+  position: relative;
   top: 1.4rem;
   left: 2.4rem;
-  position: relative;
   width: 30.9rem;
   height: 2.4rem;
   margin-bottom: 1.7rem;
+  cursor: pointer;
 `;
 const LocationSearchPlaceName = styled.h1`
   display: flex;
@@ -80,7 +105,6 @@ const LocationSearchPlaceName = styled.h1`
   width: 20rem;
   height: 1.6rem;
   color: #000000;
-  font-family: 'Pretendard';
   font-style: normal;
   font-weight: 400;
   font-size: 1.6rem;
@@ -94,6 +118,7 @@ const LocationSearchPlaceIcon = styled.img`
   left: 24rem;
   width: 2.4rem;
   height: 1.6rem;
+  cursor: ${({ edit }) => (edit ? `pointer` : null)};
 `;
 const LocationSearchPlaceTemp = styled.h1`
   display: flex;
@@ -102,7 +127,6 @@ const LocationSearchPlaceTemp = styled.h1`
   left: 28.3rem;
   width: 2.7rem;
   height: 1.6rem;
-  font-family: 'Pretendard';
   font-style: normal;
   font-weight: 700;
   font-size: 140%;
@@ -114,17 +138,16 @@ const LocationSearchPlaceTemp = styled.h1`
 const LocationDeleteIcon = styled.button`
   display: flex;
   position: absolute;
-  width: 8.6rem;
-  height: 9rem;
-  left: 22.6rem;
-  top: 0rem;
+  top: 1.9rem;
+  left: 27rem;
+  width: 2.4rem;
+  height: 2.4rem;
   border: none;
-  border-radius: 0 2rem 2rem 0;
-  background: #eb4c4c;
-  background-image: url(${DeleteIcon});
-  background-repeat: no-repeat;
-  background-position: center;
-  display: ${({ modal }) => (modal ? `none` : `show`)};
+  border-radius: 100%;
+  background: #ef1d00 url(${DeleteIcon}) no-repeat center;
+  z-index: 0.8;
+  cursor: pointer;
+  pointer-events: ${({ edit }) => (edit ? `auto` : `none`)};
 `;
 const LocationModal = styled.div`
   position: absolute;
@@ -142,7 +165,6 @@ const LocationModalQuestion = styled.h1`
   width: 13.5rem;
   height: 4.8rem;
   color: #000000;
-  font-family: 'Pretendard';
   font-style: normal;
   font-weight: 400;
   font-size: 1.6rem;
@@ -158,7 +180,6 @@ const LocationModalCancel = styled.button`
   width: 5rem;
   height: 2.1rem;
   color: #000000;
-  font-family: 'Pretendard';
   font-style: normal;
   font-weight: 400;
   font-size: 1.4rem;
@@ -167,6 +188,7 @@ const LocationModalCancel = styled.button`
   border: none;
   background: inherit;
   opacity: 0.4;
+  ${({ alert }) => (alert ? `left: 15.4rem; color: blue;` : null)};
 `;
 const LocationModalDelete = styled.button`
   position: absolute;
@@ -175,7 +197,6 @@ const LocationModalDelete = styled.button`
   width: 5rem;
   height: 2.1rem;
   color: #ef1d00;
-  font-family: 'Pretendard';
   font-style: normal;
   font-weight: 400;
   font-size: 1.4rem;
@@ -192,7 +213,9 @@ export {
   LocationModalDelete,
   LocationModalQuestion,
   LocationModalWrapper,
+  LocationNowRegion,
   LocationPinnedName,
+  LocationPinnedNowPlace,
   LocationPinnedPlace,
   LocationPinnedTemp,
   LocationResult,
