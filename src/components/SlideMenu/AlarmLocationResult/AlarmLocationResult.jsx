8865ebc8a8830c9a266eb9c169cbbcefa6a72bf8
JiAh Lee <myv_01@naver.com>
2022-09-01 22:50:58 +0900

    feat(components): handle exception about alarm

--- src/components/SlideMenu/AlarmLocationResult/AlarmLocationResult.jsx
+++ src/components/SlideMenu/AlarmLocationResult/AlarmLocationResult.jsx
@@ -1,18 +1,30 @@
 import { RegisterAlarm } from '@Apis/api';
-import setAlarmLocationAtom from '@Recoil/setAlarmLocation';
+import alarmLocationAtom from '@Recoil/alarmLocation';
+import alarmLocationListAtom from '@Recoil/alarmLocationList';
+import alarmLocationValueAtom from '@Recoil/alarmLocationValue';
 import userAtom from '@Recoil/user';
 import { useMutation } from '@tanstack/react-query';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 
 import * as S from './AlarmLocationResult.style';
 
 // eslint-disable-next-line react/prop-types
-const AlarmLocationResult = ({ result }) => {
+const AlarmLocationResult = () => {
   const token = useRecoilValue(userAtom).access;
-  const [alarmLocation, setAlarmLocation] = useRecoilState(setAlarmLocationAtom);
+  const [alarmLocation, setAlarmLocation] = useRecoilState(alarmLocationAtom);
+  const setValue = useSetRecoilState(alarmLocationValueAtom);
+  const [list, setList] = useRecoilState(alarmLocationListAtom);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    setAlarmLocation(JSON.parse(window.localStorage.getItem('alarmLocation')));
+  }, [alarmLocation]);
+
+  useEffect(() => {
+    window.localStorage.setItem('alarmLocation', JSON.stringify(alarmLocation));
+  }, [alarmLocation]);
 
   const { mutate } = useMutation(RegisterAlarm, {
     onSuccess: (data) => {
@@ -23,29 +35,27 @@ const AlarmLocationResult = ({ result }) => {
     },
   });
 
-  useEffect(() => {
-    setAlarmLocation(JSON.parse(window.localStorage.getItem('alarmLocation')));
-  }, [alarmLocation]);
-
-  useEffect(() => {
-    window.localStorage.setItem('alarmLocation', JSON.stringify(alarmLocation));
-  }, [alarmLocation]);
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    const data = {
-      keyword: {
-        city: e.target.textContent.split(' ')[0],
-        district: e.target.textContent.split(' ')[1],
-      },
-      token,
-    };
-    mutate(data);
+    if (e.target.textContent === alarmLocation) {
+      alert('이미 등록된 지역입니다.');
+      setValue({ data: '' });
+      setList({});
+    } else {
+      const data = {
+        keyword: {
+          city: e.target.textContent.split(' ')[0],
+          district: e.target.textContent.split(' ')[1],
+        },
+        token,
+      };
+      mutate(data);
+    }
   };
 
   return (
     <S.List>
-      {Object.keys(result).map((item) => (
+      {Object.keys(list).map((item) => (
         <li key={item}>
           <S.Button type="submit" onClick={handleSubmit}>
             {item}
